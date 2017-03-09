78b1297133b43586f289943f6cfbc0d34b51b686
AdolfoX Rodriguez <adolfox.rodriguez@intel.com>
2017-03-09 10:50:31 -0500

    cleaned up code a bit and updated libs

--- src/main.js
+++ src/main.js
@@ -12,6 +12,7 @@ import {
   pad,
   def,
   fallback,
+  nullFallback,
   err,
   errstr,
   errdict,
@@ -23,14 +24,6 @@ import {
   remove,
   rotate
 } from 'wircho-utilities';
-
-// Local utilities
-function nullFallback(x,y) {
-  if (def(x) && x !== null) {
-    return x;
-  }
-  return y;
-}
 
 var isDragAndDropSupported = function() {
   var div = document.createElement('div');
@@ -97,70 +90,6 @@ function uploadFileData(fileData) {
 function getImageInfo(url) {
   return apiReq({url:"/info",data:{url}});
 }
-
-/*
-function storageAvailable(type) {
-  try {
-    var storage = window[type],
-    x = '__storage_test__';
-    storage.setItem(x, x);
-    storage.removeItem(x);
-    return true;
-  }
-  catch(e) {
-    return false;   
-  }
-}
-*/
-
-// STORAGE
-/*
-function storeMessageData(data) {
-  if (storageAvailable('localStorage')) {
-    var array = JSON.parse(nullFallback(localStorage.getItem(submittedMessagesKey),"[]"));
-    array.push(data);
-    localStorage.setItem(submittedMessagesKey,JSON.stringify(array));
-  }
-}
-
-function storeClosedBubble(value) {
-  if (storageAvailable('localStorage')) {
-    if (value) {
-      localStorage.setItem(closedBubbleKey,"true");
-    }else {
-      localStorage.removeItem(closedBubbleKey);
-    }
-  }
-}
-
-function getClosedBubble() {
-  if (storageAvailable('localStorage') && localStorage.getItem(closedBubbleKey) === "true") {
-    return true;
-  }
-  return false;
-}
-
-function storeUniqueIdIfNonExistent(value) {
-  if (storageAvailable('localStorage')) {
-    var uniqueId = localStorage.getItem(uniqueIdKey);
-    if (!uniqueId) {
-      localStorage.setItem(uniqueIdKey,value);
-    }
-  }
-}
-
-function getUniqueId() {
-  if (storageAvailable('localStorage')) {
-    var uniqueId = localStorage.getItem(uniqueIdKey);
-    if (uniqueId) {
-      return uniqueId;
-    }else {
-      return undefined;
-    }
-  }
-}
-
-*/
 
 // Constants
 
@@ -232,93 +161,10 @@ const mapDispatchToProps = (dispatch) => ({
       dispatch(setInfo());
     });
   }
-/*
-  selectedPicture: (event) => {
-    event.preventDefault();
-    dispatch(finishStep(STEPS.PICTURE));
-  },
-  skippedPicture: (event) => {
-    event.preventDefault();
-    dispatch(finishStep(STEPS.PICTURE));
-  },
-  clickedLocationButton: (event,map) => {
-    event.preventDefault();
-    if (def(map) && def(map.location)) { // There is already some map info
-      dispatch(displayMap());
-    } else { // There is 
-      dispatch(enableApp(false));
-      getLocation().then(function(location) {
-        dispatch(enableApp(true));
-        dispatch(updateMap(mutate(location,{zoom:20})));
-        dispatch(displayMap());
-      },function() {
-        dispatch(enableApp(true));
-        dispatch(updateMap(MONTREAL_LOCATION));
-        dispatch(displayMap());
-      });
-    }
-  },
-  clickedMapCancelButton: (event) => {
-    event.preventDefault();
-    dispatch(hideMap(false));
-  },
-  clickedMapDoneButton: (event) => {
-    event.preventDefault();
-    dispatch(finishStep(STEPS.LOCATION));
-    dispatch(hideMap(true));
-  },
-  mapChanged: (location) => {
-    dispatch(updateMap(location));
-  },
-  clickedSubmitButton: (submitted,submitFailed) => {
-    dispatch(finishStep(STEPS.ALL));
-    uploadData().then(function(data) {
-      submitted(data);
-    }, function(error) {
-      submitFailed(error);
-    });
-  },
-  submitFailed: (error) => {
-    dispatch(undoSubmitStep());
-    alert("Something went wrong. Please try again. " + error.message);
-  },
-  submitted: (data) => {
-    storeMessageData(data);
-    clearForm();
-    dispatch(showDone());
-  },
-  clickedGetBack: (event) => {
-    event.preventDefault();
-    dispatch(clearSteps());
-  },
-  clickedCloseBubble: (event) => {
-    event.preventDefault();
-    storeClosedBubble(true);
-    dispatch(closeBubble());
-  },
-  clickedOpenBubble: (event) => {
-    event.preventDefault();
-    storeClosedBubble(false);
-    dispatch(openBubble());
-  }
-*/
 });
 
 //React classes
 const App = React.createClass({
-  /*
-  componentDidMount: function() {
-    var component = this;
-    $('#form').submit(function(event) {
-      event.preventDefault();
-      return false;
-    });
-  },
-  clickedSubmitButton: function(event) {
-    event.preventDefault();
-    this.props.clickedSubmitButton(this.props.submitted,this.props.submitFailed);
-  },
-  */
   render: function() {
     var tags = def(this.props.info) ? this.props.info.tags : undefined;
     var cats = def(this.props.info) ? this.props.info.cats : undefined;
@@ -328,57 +174,7 @@ const App = React.createClass({
         <Info loading={this.props.loading} tags={tags} cats={cats}/>
       </div>
     )
-    /*
-    return (
-      <div id="outer-content">
-        <div id="sent-content" className={classNames({hidden:!fallback(this.props.done,false)})}>
-          <div id="sent-title">Votre message a bien<br/>&eacute;t&eacute; envoy&eacute;.</div>
-          <div id="sent-message">merci!</div>
-          <a href="/" id="sent-back" onClick={this.props.clickedGetBack}>retour</a>
-        </div>
-        <div
-          id="inner-content"
-          className={classNames({disabled:!fallback(this.props.app_enabled,true),hidden:fallback(this.props.done,false)})}
-        >
-          <form
-            id="form"
-            ref="form"
-            action="/submit"
-            method="post"
-            encType="multipart/form-data"
-          >
-            <div id="bubble" className={classNames({hidden:this.props.bubble_is_closed,"inline-block":!this.props.bubble_is_closed})}>
-              Trop occup&eacute; pour appeler le 311 ou tweeter @MTL_311?<br/> Envoyez-nous vos commentaires ou plaintes concernant les services de la ville et de ses arrondissements.<br/> <span className="disclaimer">Nous n'avons aucune affiliation avec la ville de Montr&eacute;al.</span>
-              <div id="bubble-border"/>
-              <button id="bubble-close" onClick={this.props.clickedCloseBubble}/>
-            </div><br/>
-            <div id="header" className={classNames({"no-bubble":this.props.bubble_is_closed})}>
-              CHER MTL,
-              <button id="bubble-open" onClick={this.props.clickedOpenBubble} className={classNames({hidden:!this.props.bubble_is_closed})}>?</button>
-            </div>
-            <Steps
-              step={this.props.step}
-              map={this.props.map}
-              text={this.props.text}
-              selectedPicture={this.props.selectedPicture}
-              skippedPicture={this.props.skippedPicture}
-              clickedLocationButton={this.props.clickedLocationButton}
-              clickedSubmitButton={this.clickedSubmitButton}
-            />
-            <MapCanvas
-              map={this.props.map}
-              mapChanged={this.props.mapChanged}
-            />
-            <MapOverlay
-              map={this.props.map}
-              clickedMapCancelButton={this.props.clickedMapCancelButton}
-              clickedMapDoneButton={this.props.clickedMapDoneButton}
-            />
-          </form>
-        </div>
-      </div>
-    );
-    */
+    
   }
 });
 
@@ -484,156 +280,6 @@ const Info = React.createClass({
     )
   }
 });
-
-/*
-const Steps = React.createClass({
-  render: function() {
-    var nextStep = this.props.step + 1;
-    return (<ul id="steps" className={(def(this.props.map) && this.props.map.visible) ? "hidden" : "block"}>
-      <PictureStep 
-        active={nextStep >= STEPS.PICTURE && this.props.step < STEPS.ALL}
-        done={nextStep > STEPS.PICTURE} 
-        selectedPicture={this.props.selectedPicture}
-        skippedPicture={this.props.skippedPicture}
-      />
-      <LocationStep
-        active={nextStep >= STEPS.LOCATION && this.props.step < STEPS.ALL}
-        done={nextStep > STEPS.LOCATION}
-        map={this.props.map}
-        clickedLocationButton={this.props.clickedLocationButton}
-      />
-      <MessageStep
-        active={nextStep >= STEPS.MESSAGE && this.props.step < STEPS.ALL}
-        done={nextStep > STEPS.MESSAGE}
-        clickedSubmitButton={this.props.clickedSubmitButton}
-      />
-    </ul>);
-  }
-});
-
-const Step = React.createClass({
-  render: function() {
-    return (<li id={this.props.id} className={classNames({disabled:!this.props.active,done:this.props.done})}>{this.props.children}</li>);
-  }
-})
-
-const PictureStep = React.createClass({
-  render: function() {
-    return (<Step active={this.props.active} done={this.props.done}>
-      <input type="file" id="picture" name="picture" accept="image/*" onChange={this.props.selectedPicture}/>
-      <div id="orskip" className={classNames({hidden:this.props.done})}>
-        <button id="skip" onClick={this.props.skippedPicture}>pas maintenant</button>
-      </div>
-    </Step>);
-  }
-});
-
-const LocationStep = React.createClass({
-  render: function() {
-    var clickedLocationButton = projff(this.props.clickedLocationButton,undefined,()=>(this.props.map));
-    return (<Step active={this.props.active} done={this.props.done}>
-      <button id="pin-location" disabled={!this.props.active} onClick={clickedLocationButton}>trouver mon emplacement</button>
-      <Info dictionary={def(this.props.map) ? this.props.map.savedLocation : undefined} prefix="location" />
-    </Step>);
-  }
-});
-
-const Info = React.createClass({
-  render: function() {
-    var dictionary = this.props.dictionary;
-    var inputs = [];
-    var prefix = this.props.prefix;
-    for (var key in dictionary) {
-      if (dictionary.hasOwnProperty(key)) {
-        var id = prefix + "-" + key;
-        inputs.push(<input type="hidden" key={key} value={dictionary[key]} id={id} name={id}/>);
-      }
-    }
-    var hiddenStyle = {
-      display:"none"
-    }; 
-    return (
-      <div style={hiddenStyle}>{inputs}</div>
-    )
-  }
-})
-
-const MessageStep = React.createClass({
-  componentDidUpdate: function(prevProps) {
-    if (!prevProps.active && this.props.active) {
-      $("#message").focus();
-      $("html, body").animate({
-        scrollTop: $("#message-step").offset().top - 5
-      }, 100);
-    }
-  },
-  render: function() {
-    return (<Step active={this.props.active} done={this.props.done} id="message-step">
-      <textarea
-        id="message"
-        name="message"
-        className={classNames({tall:this.props.active,short:!this.props.active})}
-        placeholder="message (optionnel)"
-        disabled={!this.props.active}
-        maxLength={FORM.MAX_MESSAGE}
-      />
-      <div id="merci" className={classNames({hidden:!this.props.active})}>
-        MERCI,<br/>
-        <input type="text" id="name" name="name" maxLength={FORM.MAX_NAME} placeholder="email ou nom (optionnel)" disabled={!this.props.active}/>
-      </div>
-      <button
-        type="submit"
-        id="submit"
-        disabled={!this.props.active}
-        onClick={this.props.clickedSubmitButton}
-      >envoyer</button>
-    </Step>);
-  }
-});
-
-const isTouchDevice = 'ontouchstart' in document.documentElement;
-
-const MapCanvas = React.createClass({
-  componentDidUpdate: function(prevProps) {
-    if (def(prevProps.map) && prevProps.map.visible) {
-      return;
-    }
-    if (def(this.props.map) && this.props.map.visible) {
-      var center = new google.maps.LatLng(this.props.map.location.latitude,this.props.map.location.longitude);
-      var zoom = this.props.map.location.zoom;
-      var mapOptions = {
-        center: center,
-        zoom: zoom
-      };
-      var map = new google.maps.Map(document.getElementById("map-canvas"),mapOptions);
-      if (isTouchDevice) {
-        var handler = function() {
-          center = map.getCenter();
-          this.props.mapChanged({zoom:map.getZoom(),latitude:center.lat(),longitude:center.lng()});
-        }.bind(this);
-        map.addListener("zoom_changed",handler);
-        map.addListener("dragend",handler);
-      }else {
-        map.addListener("zoom_changed",function() {
-          map.setCenter(center);
-          this.props.mapChanged({zoom:map.getZoom()});
-        }.bind(this));
-        map.addListener("dragend",function() {
-          center = map.getCenter();
-          this.props.mapChanged({latitude:center.lat(),longitude:center.lng()});
-        }.bind(this));
-      }
-    }
-  },
-  render: function() {
-    if (def(this.props.map) && this.props.map.visible) {
-      return <div id="map-canvas-container"><div id="map-canvas" /></div>;
-    }else {
-      return false;
-    }
-  }
-});
-*/
 
 //React / Redux connection and render
 const store = createStore(app);
